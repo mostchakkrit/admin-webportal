27740b999b635c0fcbf62c1689da88ab5af0e672
mostchakkrit <chakkrit@agiletech.dev>
2023-04-12 14:37:00 +0700

    healthLink

--- pages/steps/health-link.tsx
+++ pages/steps/health-link.tsx
@@ -3,6 +3,7 @@ import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 /* img */
 import Image from "next/image";
 import Head from "next/head";
+import Link from "next/link";
 
 function healthLink() {
   return (
@@ -19,9 +20,26 @@ function healthLink() {
       </div> */}
       <div className="grid lg:grid-cols-2 xs:grid-cols-1 gap-5">
         {/* box 1 */}
-        <div>รูปภาพ</div>
+        <div className="w-full">
+          <Image
+            src="/images/stepsImg/healthlink.png"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-full h-auto"
+            alt=""
+          />
+          <div>
+            <button
+              type="button"
+              className="w-full bg-white text-left border rounded-full border-[#4DD0D5] hover:scale-x-105 transition p-3"
+            >
+              เพิ่มเติม
+            </button>
+          </div>
+        </div>
         {/* box 2 */}
-        <div>
+        <div className="w-full">
           <div className="flex flex-warp justify-between gap2">
             <div>1. หน้าจอจะแสดงข้อมูล Overview</div>
             <div>
@@ -29,7 +47,12 @@ function healthLink() {
               {">"}
             </div>
           </div>
-          <div className="flex mt-5">text</div>
+          <div className="flex flex-wrap mt-5 break-all">
+            <p className="">
+              xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+              xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+            </p>
+          </div>
         </div>
       </div>
     </>
