430d8c63e40205545af6a09e5968e8eb54a78964
mostchakkrit <chakkrit@agiletech.dev>
2023-05-02 13:43:31 +0700

    manual

--- pages/manual.tsx
+++ pages/manual.tsx
@@ -2,12 +2,8 @@ import Head from "next/head";
 import React from "react";
 
 function Manual() {
-  return (
-    <>
-      <Head>
-        <title>คู่มือการใช้งานss</title>
-      </Head>
-    </>
-  );
+  return <>
+  
+  </>;
 }
 export default Manual;
