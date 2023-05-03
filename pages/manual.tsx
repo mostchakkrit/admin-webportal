395ba6092e5009b0de5a1e404e8ee6e3ecfb04fb
ahyoyo21 <igetken89@gmail.com>
2023-05-03 18:45:40 +0700

    day2

--- pages/manual.tsx
+++ pages/manual.tsx
@@ -1,36 +1,56 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
+import { dataSlide } from "../public/data/dataset";
 
 function Manual() {
   const [currentSlide, setCurrentSlide] = useState(1);
 
   const SlideChange = (index: number) => {
-    setCurrentSlide(index+1);
-  }
-  return <>
-  <Carousel width={"80%"} showThumbs={false} onChange={SlideChange}>
-<div>
-  <img src="/images/manual/regisManual.jpg" alt="regisMan" id="regisMan"/>
-  <p className="legend">คู่มือการลงทะเบียน</p>
-</div>
-<div>
-  <img src="/images/manual/loginManual.jpg" alt="loginMan" id="loginMan"/>
-  <p className="legend">คู่มือการเข้าสู่ระบบ</p>
-</div>
-<div>
-  <img src="/images/manual/dowManual.jpg" alt="dowMan" id="dowMan"/>
-  <p className="legend">คู่มือการดาวน์โหลด</p>
-</div>
-<div>
-  <img src="/images/manual/Manual4.jpg" alt="Man1" id="Man1"></img>
-  <p className="legend">คู่มือการใช้งาน</p>
-</div>
-</Carousel>
-<div className="Infomation">
-  <div><p>This is page {currentSlide}</p> </div>
+    setCurrentSlide(index + 1);
+    changeData(index + 1);
+  };
+  const info: any = useRef();
 
-</div>
-</>;
+  const changeData = (num: number) => {
+    const result = dataSlide.find((data: any) => data.id === num);
+    info.current.innerHTML = `<div class="title">page${result.text}of4</div>
+    <div><h1>${result.title === undefined ? "" : result.title}</h1></div>
+    <div><h3><a href="www.facebook.com/${result.facebook}">${result.facebook}</a></h3></div>`;
+  };
+  /* ${result.id} */
+  {/* <img src="${result.img}"></img> */}
+  return (
+    <>
+      <Carousel width={"80%"} showThumbs={false} onChange={SlideChange}>
+        <div>
+          <img
+            src="/images/manual/regisManual.jpg"
+            alt="regisMan"
+            id="regisMan"
+          />
+          <p className="legend">คู่มือการลงทะเบียน</p>
+        </div>
+        <div>
+          <img
+            src="/images/manual/loginManual.jpg"
+            alt="loginMan"
+            id="loginMan"
+          />
+          <p className="legend">คู่มือการเข้าสู่ระบบ</p>
+        </div>
+        <div>
+          <img src="/images/manual/dowManual.jpg" alt="dowMan" id="dowMan" />
+          <p className="legend">คู่มือการดาวน์โหลด</p>
+        </div>
+        <div>
+          <img src="/images/manual/Manual4.jpg" alt="Man1" id="Man1"></img>
+          <p className="legend">คู่มือการใช้งาน</p>
+        </div>
+      </Carousel>
+
+      <div className="Infomation" ref={info}></div>
+    </>
+  );
 }
 export default Manual;
---
+++ public/data/dataset.tsx
@@ -0,0 +1,23 @@
+export const dataSlide: any = [
+  {
+    id: 1,
+    img: "/images/manual/regisManual.jpg",
+    text: "1",
+    title:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates, magnam repellendus? Cumque, iusto repudiandae eius eum dolores, magni fugit tempore ut soluta aspernatur maxime perspiciatis corrupti fuga autem, laborum quae dolore error! Quae illum ducimus molestias modi quis saepe iusto rerum voluptas ratione ipsam? Repellendus odio ad dolorum qui quasi eligendi aliquid ipsum sint molestiae voluptatibus? Illo sunt numquam molestias reiciendis? Dignissimos aliquam quos molestiae, quas quod repudiandae similique eligendi quidem officiis ipsa, commodi quae tempore eos dicta laboriosam itaque accusantium sed fugit neque? Inventore omnis iste consequatur ducimus at. Nam ullam laboriosam repellat asperiores saepe consectetur itaque facilis ipsum?",
+  },
+  {
+    id: 2,
+    text: "2",
+    facebook: "คนที่2"
+  },
+  {
+    id: 3,
+    text: "3",
+    facebook: "คนที่3"
+  },
+  {
+    id: 4,
+    text: "4",
+  },
+];
